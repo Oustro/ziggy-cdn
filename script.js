dae028960404b906fe4ca26d7c2e426e90152d40
Jacob Thomas <55421470+jacobtt21@users.noreply.github.com>
2023-06-11 13:21:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const renderZiggy = async (e) => {
   let html = `
-    <button
-      class="fixed bottom-10 right-10 bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-md transition-transform duration-300 ease-in-out transform hover:scale-105"
-      onclick="openZiggy()"
-    >
-      Open Ziggy
-    </button>
+    <div class="fixed bg-gradient-to-r shadow bottom-10 right-10 from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% rounded-full p-[1px]">
+      <div
+        class="bg-slate-100 hover:bg-slate-200 text-white p-4 rounded-full transition-transform duration-300 ease-in-out transform hover:scale-105"
+        onclick="openZiggy()"
+      >
+        <img src="https://ziggy.oustro.xyz/logo.svg" alt="Ziggy Logo" width="40" height="40">
+      </div>
+    </div>
 
     <div
       id="ziggyModal"
-      class="fixed opacity-0 pointer-events-none flex justify-center items-center z-50 bottom-24 right-10 w-96 h-4/6 overflow-hidden bg-white border border-gray-300 rounded-md transition-opacity duration-300 ease-in-out"
+      class="fixed opacity-0 pointer-events-none flex justify-center items-center z-50 bottom-32 right-10 w-96 h-4/6 overflow-hidden bg-white border border-gray-300 rounded-md transition-opacity duration-300 ease-in-out"
     >
       <div class="w-full h-full">
         <span
@@ -27,17 +29,17 @@ const renderZiggy = async (e) => {
     </div>
   `;
 
-  document.getElementById("app").innerHTML = html;
-};
+    document.getElementById("app").innerHTML = html;
+  };
 
-function openZiggy() {
-  var ziggyModal = document.getElementById("ziggyModal");
-  ziggyModal.classList.add("opacity-100");
-  ziggyModal.classList.remove("pointer-events-none");
-}
+  function openZiggy() {
+    var ziggyModal = document.getElementById("ziggyModal");
+    ziggyModal.classList.add("opacity-100");
+    ziggyModal.classList.remove("pointer-events-none");
+    }
 
-function closeZiggy() {
-  var ziggyModal = document.getElementById("ziggyModal");
-  ziggyModal.classList.remove("opacity-100");
-  ziggyModal.classList.add("pointer-events-none");
-}
+  function closeZiggy() {
+    var ziggyModal = document.getElementById("ziggyModal");
+    ziggyModal.classList.remove("opacity-100");
+    ziggyModal.classList.add("pointer-events-none");
+  }
