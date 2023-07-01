d412c32e699015e810cb288a860268b1598597f7
Jacob Thomas <55421470+jacobtt21@users.noreply.github.com>
2023-06-30 22:52:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,30 +2,41 @@ const renderZiggy = async (e) => {
   let html = `
     <div class="fixed bg-gradient-to-r shadow bottom-10 right-10 from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% rounded-full p-[1px]">
       <div
-        id="ziggyButton"
-        class="bg-slate-100 hover:bg-slate-200 text-white p-4 rounded-full transition-transform duration-300 ease-in-out transform hover:scale-105"
-        onclick="openZiggy()"
+      id="ziggyButton"
+      class="bg-slate-100 hover:bg-slate-200 text-white p-4 rounded-full transition-transform duration-300 ease-in-out transform hover:scale-105"
+      onclick="openZiggy()"
       >
         <img src="https://ziggy.oustro.xyz/logo.svg" alt="Ziggy Logo" width="40" height="40">
       </div>
     </div>
 
     <div
-      id="ziggyModal"
-      class="fixed opacity-0 pointer-events-none flex justify-center items-center z-50 bottom-32 right-10 w-96 h-4/6 overflow-hidden bg-white border border-gray-300 rounded-md transition-opacity duration-300 ease-in-out"
+    class="fixed bottom-32 p-2 right-12 rounded-md bg-slate-200 shadow"
+    >
+      <div class="w-full rounded-lg h-full">
+        Hi, can I ask you some questions?
+      </div>
+    </div>
+
+    <div
+    id="ziggyModal"
+    class="fixed opacity-0 pointer-events-none flex justify-center items-center z-50 bottom-32 right-10 w-96 h-4/6 overflow-hidden bg-white border border-gray-300 rounded-md transition-opacity duration-300 ease-in-out"
     >
       <div class="w-full h-full">
         <span
-          class="absolute top-4 right-4 text-gray-500 cursor-pointer"
-          onclick="closeZiggy()"
-          >&times;</span
+        class="absolute top-4 right-4 text-gray-500 cursor-pointer"
+        onclick="closeZiggy()"
+        >
+          &times;
+        </span
         >
         <iframe
-          src="https://ziggy.oustro.xyz/t/${e}?embedded=true"
-          width="100%"
-          height="100%"
-          frameborder="0"
-        ></iframe>
+        src="https://ziggy.oustro.xyz/t/${e}?embedded=true"
+        width="100%"
+        height="100%"
+        frameborder="0"
+        >
+        </iframe>
       </div>
     </div>
   `;
